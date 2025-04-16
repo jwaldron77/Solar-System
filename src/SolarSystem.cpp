068ae9c77921785e38e66c5758eb4b118c179471
jwaldron77 <152323293+jwaldron77@users.noreply.github.com>
2025-04-16 18:37:56 -0400

    changed location of texture

--- src/SolarSystem.cpp
+++ src/SolarSystem.cpp
@@ -55,7 +55,7 @@ std::vector<CelestialBody> getSolarSystem()
   CelestialBody JoeWorld
   (
     "JoeWorld",
-    "PlanetTexture.jpg",
+    "planetTexture.jpg",
     0.3301      * pow(10, 24),
     6.083       * pow(10, 10),
     5.429       * pow(10, 3),
